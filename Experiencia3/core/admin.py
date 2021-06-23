cd7ac89b65172edfb0f81368aeb2ec806bb34df4
francisco baeza carrasco <fran.baezac@duocuc.cl>
2021-06-22 20:30:54 -0400

    se implementa la base de datos

--- Experiencia3/core/admin.py
+++ Experiencia3/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Cuenta,Publicacion
 # Register your models here.
+
+admin.site.register(Cuenta)
+admin.site.register(Publicacion)
+
+
--- Experiencia3/core/models.py
+++ Experiencia3/core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class Cuenta(models.Model):
+    Correo = models.CharField(primary_key=True,max_length=100,verbose_name='Correo')
+    Nombre = models.CharField(max_length=30,verbose_name='Nombre')
+    Contraseña = models.CharField(max_length=30,verbose_name='Contraseña')
+
+    def __str__(self):
+        return (self.Correo)
+
+class Publicacion(models.Model):
+    Correo = models.CharField(primary_key=True,max_length=100,verbose_name='Correo')
+    Nombre = models.CharField(max_length=30,verbose_name='Nombre')
+    Categoria = models.CharField(max_length=15,verbose_name='Categoria')
+    Detalles = models.CharField(max_length=150,verbose_name='Detalles')
+    cuenta = models.ForeignKey(Cuenta, on_delete=CASCADE)
+
+    def __str__(self):
+        return (self.Categoria)
+
+    
